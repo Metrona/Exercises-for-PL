c5c096e1f9bd39d843b9fa5707bfc8dde5d5c587
Metrona <justin14knight@gmail.com>
2021-07-19 11:54:08 +0700

    Create Book.java

---
+++ Exercises/2-3/Book.java
@@ -0,0 +1,67 @@
+public class Book
+{
+    private String isbn;
+    private String name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+
+    public Book(String isbn, String name, Author author, double price)
+    {
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    
+    public Book(String isbn, String name, Author author, double price, int qty)
+    {
+        this(isbn,name,author,price);
+        this.qty = qty;
+    }
+
+    public String getISBN()
+    {
+        return isbn;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Author getAuthor()
+    {
+        return author;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getQty()
+    {
+        return qty;
+    }
+
+    public void setQty(int qty)
+    {
+        this.qty = qty;
+    }
+
+    public String toString()
+    {
+        return "Book[" + "name=" + name + ", authors=" + author.toString() + ", price=" + price + ", qty=" + qty + ']';
+    }
+
+    public String getAuthorName()
+    {
+        return author.getName();
+    }
+}
